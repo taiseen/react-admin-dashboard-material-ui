72510c97395bfe4b35edb2ba446af988e251a2bb
taiseen <taiseen.cse@gmail.com>
2022-11-18 13:56:02 +0600

    readme edit ==> OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material'; // reset all default css
 import { ColorModeContext, useMode } from './styles/theme';
 
 const App = () => {
@@ -10,7 +10,9 @@ const App = () => {
     <ColorModeContext.Provider value={coloMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <main className="app">App</main>
+        <main className="app">
+          <div className="content"></div>
+        </main>
       </ThemeProvider>
     </ColorModeContext.Provider>
   )
